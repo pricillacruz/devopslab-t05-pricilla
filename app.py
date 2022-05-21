075c14beabab25ed18c576f5a622e84005bece7d
Pricilla Souza Cruz <61021558+pricillacruz@users.noreply.github.com>
2022-05-21 13:11:50 -0300

    Dockerizando a app

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ csrf = CSRFProtect(app)
 @app.route("/")
 def pagina_inicial():
     return "Hello world, I'm Pricilla Souza :)"
+
+if __name__ == '__main__':
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)    
